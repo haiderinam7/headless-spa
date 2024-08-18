ce5dd071fcef707cdb1bb60c30adae46a9c15a41
Haider <157199010+haiderinam7@users.noreply.github.com>
2024-08-18 06:07:47 +0000

    Setup fonts and global css

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Lato, Open_Sans } from "next/font/google";
+import "../styles/styles.sass";
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"],
+  variable: "--font-lato",
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+});
+
+export const metadata = {
+  title: "Headless Blog",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body
+      className={`${openSans.className} ${openSans.variable} ${lato.variable}`}
+    >
+      {children}
+    </body>
+  </html>
+);
+
+export default RootLayout;
